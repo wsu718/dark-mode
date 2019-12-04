a9642b6f2599c17b90d512726ef3de59ff6f7721
William Sulinski <w@williamsulinski.com>
2019-12-04 14:18:40 -0800

    MVP

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,13 @@
+import { useLocalStorage } from './useLocalStorage';
+import { useEffect } from 'react';
+
+export const useDarkMode = (initialValue) => {
+    const [darkMode, setDarkMode] = useLocalStorage(initialValue);
+
+    useEffect(() => {
+        darkMode ? document.body.classList.add('dark-mode') : document.body.classList.remove('dark-mode');
+    }, [darkMode])
+
+    return [darkMode, setDarkMode]
+}
+
